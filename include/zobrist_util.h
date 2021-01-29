862db804127b9a4a5bdb0f9b26ee1eb558b0ad84
Connor McMonigle <connormcmonigle@gmail.com>
2021-01-29 10:25:23 -0800

    fix formatting

--- include/zobrist_util.h
+++ include/zobrist_util.h
@@ -5,15 +5,16 @@
 #include <random>
 
 namespace zobrist{
-  using hash_type = std::uint64_t;
-  constexpr unsigned int seed = 0;
+
+using hash_type = std::uint64_t;
+constexpr unsigned int seed = 0;
   
-  inline hash_type random_bit_string(){
-    static std::mt19937 gen{seed};
-    constexpr hash_type a = std::numeric_limits<hash_type>::min();
-    constexpr hash_type b = std::numeric_limits<hash_type>::max();
-    std::uniform_int_distribution<hash_type> dist(a, b);
-    return dist(gen);
-  }
+inline hash_type random_bit_string(){
+  static std::mt19937 gen{seed};
+  constexpr hash_type a = std::numeric_limits<hash_type>::min();
+  constexpr hash_type b = std::numeric_limits<hash_type>::max();
+  std::uniform_int_distribution<hash_type> dist(a, b);
+  return dist(gen);
+}
 
 }
