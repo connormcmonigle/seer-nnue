08291bd05fc6501b073217fb4f9b9b7747255d9d
Connor McMonigle <connormcmonigle@gmail.com>
2021-03-18 20:19:06 -0700

    fix bench behavior

--- src/seer.cc
+++ src/seer.cc
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <string>
+#include <cstdint>
 
 #include <uci.h>
 
 
 int main(int argc, char* argv[]){
   const std::string argument = [&]{
+    if(argc <= 1){ return std::string{}; }
     std::string result{};
-    for(int i(1); i < argc; ++i){ result += argv[i]; }
+    for(int i(1); i < argc; ++i){
+      result += argv[i];
+      result += " ";
+    }
+    result.pop_back();
     return result;
   }();
 
   engine::uci u{};
-  if(!argument.empty()){ u.uci_loop(argument); return 0; }
-
+  if(!argument.empty()){
+    u.uci_loop(argument);
+    std::exit(0);
+  }
+  
   while(true){
     std::string line{}; std::getline(std::cin, line);
     u.uci_loop(line);
