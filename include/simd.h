6493513eaef2f0441e39210104ed207e858b61b4
Connor McMonigle <connormcmonigle@gmail.com>
2021-04-27 00:36:28 -0700

    add support for older processors

--- include/simd.h
+++ include/simd.h
@@ -1,14 +1,20 @@
 #pragma once
 #include <type_traits>
 
-#ifdef __AVX2__
+#ifdef __AVX__
 #include <immintrin.h>
+#elif __SSE__
+#include <xmmintrin.h>
 #endif
 
 
 namespace simd{
 
+#ifdef __AVX__
 constexpr size_t alignment = 32;
+#elif __SSE__
+constexpr size_t alignment = 16;
+#endif
 
 template<typename T, size_t N>
 inline T dot_product_(const T* a, const T* b){
@@ -22,8 +28,8 @@ inline T dot_product_(const T* a, const T* b){
 
 template<typename T, size_t N>
 inline T dot_product(const T* a, const T* b){
-#ifdef __AVX2__
   if constexpr(std::is_same_v<T, float>){
+#ifdef __AVX__
     static_assert(alignment % sizeof(T) == 0, "alignment must be divisible by sizeof(T)");
     constexpr size_t num_units = 2;
     constexpr size_t per_unit = alignment / sizeof(T);
@@ -34,12 +40,12 @@ inline T dot_product(const T* a, const T* b){
 
     for(size_t i(0); i < N; i += per_iteration, a += per_iteration, b += per_iteration){
       {
-        const __m256 a_0 = _mm256_load_ps(a + 0*8); const __m256 b_0 = _mm256_load_ps(b + 0*8);
+        const __m256 a_0 = _mm256_load_ps(a + 0*per_unit); const __m256 b_0 = _mm256_load_ps(b + 0*per_unit);
         sum_0 = _mm256_fmadd_ps(a_0, b_0, sum_0);
       }
 
       {
-        const __m256 a_1 = _mm256_load_ps(a + 1*8); const __m256 b_1 = _mm256_load_ps(b + 1*8);
+        const __m256 a_1 = _mm256_load_ps(a + 1*per_unit); const __m256 b_1 = _mm256_load_ps(b + 1*per_unit);
         sum_1 = _mm256_fmadd_ps(a_1, b_1, sum_1);
       }
     }
@@ -53,14 +59,52 @@ inline T dot_product(const T* a, const T* b){
     const __m128 reduced_1 = _mm_add_ss(reduced_2, _mm_shuffle_ps(reduced_2, reduced_2, 0x1));
     const T sum = _mm_cvtss_f32(reduced_1);
     return sum;
+#elif __SSE__
+    static_assert(alignment % sizeof(T) == 0, "alignment must be divisible by sizeof(T)");
+    constexpr size_t num_units = 4;
+    constexpr size_t per_unit = alignment / sizeof(T);
+    constexpr size_t per_iteration = per_unit * num_units;
+    static_assert(N % per_iteration == 0, "N must be divisible by per_iteration");
+    __m128 sum_0 = _mm_setzero_ps();
+    __m128 sum_1 = _mm_setzero_ps();
+    __m128 sum_2 = _mm_setzero_ps();
+    __m128 sum_3 = _mm_setzero_ps();
 
+    for(size_t i(0); i < N; i += per_iteration, a += per_iteration, b += per_iteration){
+      {
+        const __m128 a_0 = _mm_load_ps(a + 0*per_unit); const __m128 b_0 = _mm_load_ps(b + 0*per_unit);
+        sum_0 = _mm_add_ps(_mm_mul_ps(a_0, b_0), sum_0);
+      }
+
+      {
+        const __m128 a_1 = _mm_load_ps(a + 1*per_unit); const __m128 b_1 = _mm_load_ps(b + 1*per_unit);
+        sum_1 = _mm_add_ps(_mm_mul_ps(a_1, b_1), sum_1);
+      }
+      
+      {
+        const __m128 a_2 = _mm_load_ps(a + 2*per_unit); const __m128 b_2 = _mm_load_ps(b + 2*per_unit);
+        sum_2 = _mm_add_ps(_mm_mul_ps(a_2, b_2), sum_2);
+      }
+
+      {
+        const __m128 a_3 = _mm_load_ps(a + 3*per_unit); const __m128 b_3 = _mm_load_ps(b + 3*per_unit);
+        sum_3 = _mm_add_ps(_mm_mul_ps(a_3, b_3), sum_3);
+      }
+    }
+
+    const __m128 reduced_4  = _mm_add_ps(_mm_add_ps(sum_0, sum_1), _mm_add_ps(sum_2, sum_3));
+    // adds lower 2 float elements to the upper 2
+    const __m128 reduced_2 = _mm_add_ps(reduced_4, _mm_movehl_ps(reduced_4, reduced_4));
+    // adds 0th float element to 1st float element
+    const __m128 reduced_1 = _mm_add_ss(reduced_2, _mm_shuffle_ps(reduced_2, reduced_2, 0x1));
+    const T sum = _mm_cvtss_f32(reduced_1);
+    return sum;
+#else
+    return dot_product_<T, N>(a, b);
+#endif
   }else{
     return dot_product_<T, N>(a, b);
   }
-#else
-  return dot_product_<T, N>(a, b);
-#endif
-
 }
 
 }
