3e3eb74602a11f13bce4e5e4a1899069df4f6059
Connor McMonigle <connormcmonigle@gmail.com>
2021-07-01 15:23:43 -0700

    test

--- include/search_constants.h
+++ include/search_constants.h
@@ -61,7 +61,7 @@ inline constexpr score_type mate_score = max_mate_score - (max_depth + max_depth
 
 inline constexpr score_type draw_score = 0;
 
-inline constexpr score_type aspiration_delta = 30;
+inline constexpr score_type aspiration_delta = 20;
 
 inline constexpr score_type stability_threshold = 50;
 
