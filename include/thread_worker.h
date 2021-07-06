aad9743d8d6175932d990a73dd167ecc87efb6cc
connormcmonigle <connormcmonigle@gmail.com>
2021-07-06 10:57:37 -0700

    no-reset-cache (#32)
    bench: 2452272
    ELO   | 6.18 +- 4.58 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 11080 W: 2880 L: 2683 D: 5517
    ELO   | 4.23 +- 3.22 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 3.02 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 16352 W: 3094 L: 2895 D: 10363

--- include/thread_worker.h
+++ include/thread_worker.h
@@ -552,7 +552,6 @@ struct thread_worker {
       internal.best_move.store(bd.generate_moves().begin()->data);
       internal.stack = search::stack(hist, bd);
       internal.hh.clear();
-      internal.cache.clear();
     });
   }
 
