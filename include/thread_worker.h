9267fbc1ddbf0dfa303a63d7b035e3f03db29f50
connormcmonigle <connormcmonigle@gmail.com>
2021-07-17 16:33:13 -0700

    test

--- include/thread_worker.h
+++ include/thread_worker.h
@@ -427,7 +427,10 @@ struct thread_worker {
         auto full_width = [&] { return -pv_search<is_pv>(ss.next(), eval_, bd_, -beta, -alpha, next_depth); };
         auto zero_width = [&](const search::depth_type& zw_depth) { return -pv_search<false>(ss.next(), eval_, bd_, -alpha - 1, -alpha, zw_depth); };
 
-        search::score_type zw_score{};
+        if (is_pv && idx == 0) { return full_width(); }
+
+        search::depth_type lmr_depth;
+        search::score_type zw_score;
 
         // step 12. late move reductions
         const bool try_lmr = !is_check && (mv.is_quiet() || see_value < 0) && idx >= 2 && (depth >= external.constants->reduce_depth());
@@ -445,12 +448,12 @@ struct thread_worker {
 
           reduction = std::max(reduction, 0);
 
-          const search::depth_type lmr_depth = std::max(1, next_depth - reduction);
+          lmr_depth = std::max(1, next_depth - reduction);
           zw_score = zero_width(lmr_depth);
         }
 
         // search again at full depth if necessary
-        if (!try_lmr || (zw_score > alpha)) { zw_score = zero_width(next_depth); }
+        if (!try_lmr || (zw_score > alpha && lmr_depth < next_depth)) { zw_score = zero_width(next_depth); }
 
         // search again with full window on pv nodes
         return (is_pv && (alpha < zw_score && zw_score < beta)) ? full_width() : zw_score;
