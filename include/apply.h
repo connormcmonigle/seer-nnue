f845d236e54ae593f148fb976b0d8a4112d0af21
connormcmonigle <connormcmonigle@gmail.com>
2021-07-29 17:43:13 -0700

    clean up

--- include/apply.h
+++ include/apply.h
@@ -1,10 +1,11 @@
 #pragma once
 #include <tuple>
+#include <type_traits>
 
 namespace util {
 
-template <typename... Ts, typename F, size_t... I>
-constexpr void apply_impl(std::tuple<Ts...>& data, F&& f, std::index_sequence<I...>) {
+template <typename T, typename F, size_t... I>
+constexpr void apply_impl(T&& data, F&& f, std::index_sequence<I...>) {
   auto map = [&f](auto&& x) {
     f(x);
     return 0;
@@ -12,24 +13,9 @@ constexpr void apply_impl(std::tuple<Ts...>& data, F&& f, std::index_sequence<I.
   [[maybe_unused]] const auto ignored = {map(std::get<I>(data))...};
 }
 
-template <typename... Ts, typename F>
-constexpr void apply(std::tuple<Ts...>& data, F&& f) {
-  apply_impl(data, std::forward<F>(f), std::make_index_sequence<sizeof...(Ts)>{});
+template <typename T, typename F>
+constexpr void apply(T&& data, F&& f) {
+  apply_impl(std::forward<T>(data), std::forward<F>(f), std::make_index_sequence<std::tuple_size_v<std::decay_t<T>>>{});
 }
-
-template <typename... Ts, typename F, size_t... I>
-constexpr void apply_impl(const std::tuple<Ts...>& data, F&& f, std::index_sequence<I...>) {
-  auto map = [&f](auto&& x) {
-    f(x);
-    return 0;
-  };
-  [[maybe_unused]] const auto ignored = {map(std::get<I>(data))...};
-}
-
-template <typename... Ts, typename F>
-constexpr void apply(const std::tuple<Ts...>& data, F&& f) {
-  apply_impl(data, std::forward<F>(f), std::make_index_sequence<sizeof...(Ts)>{});
-}
-
 
 }  // namespace util
