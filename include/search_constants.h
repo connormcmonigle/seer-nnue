2d1595c7d00551b0bdfc5a50e28fe3e7c61ae652
connormcmonigle <connormcmonigle@gmail.com>
2021-07-31 16:34:19 -0700

    prob-prune-tweak-3 (#62)
    bench: 2236605
    ELO   | 0.71 +- 2.13 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | -2.95 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 45080 W: 10022 L: 9930 D: 25128
    ELO   | 1.95 +- 3.02 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.97 (-2.94, 2.94) [-4.00, 1.00]
    GAMES | N: 15512 W: 2411 L: 2324 D: 10777

--- include/search_constants.h
+++ include/search_constants.h
@@ -113,9 +113,9 @@ struct fixed_constants {
 
   constexpr score_type singular_double_extension_margin() const { return 160; }
 
-  constexpr depth_type prob_prune_depth_margin() const { return 3; }
+  constexpr depth_type prob_prune_depth_margin() const { return 2; }
 
-  constexpr score_type prob_prune_margin() const { return 512; }
+  constexpr score_type prob_prune_margin() const { return 768; }
 
   constexpr score_type futility_margin(const depth_type& depth) const {
     assert(depth > 0);
