f5703be49b0789552922e3fa23c18e7d6bbbe6b3
connormcmonigle <connormcmonigle@gmail.com>
2021-08-16 09:07:29 -0700

    tweak

--- include/thread_worker.h
+++ include/thread_worker.h
@@ -471,7 +471,7 @@ struct thread_worker {
           // adjust reduction
           if (bd_.is_check()) { --reduction; }
           if (bd.is_passed_push(mv)) { --reduction; }
-          if (improving && depth <= 8) { --reduction; }
+          if (improving && depth <= 6) { --reduction; }
           if (!is_pv) { ++reduction; }
           if (did_double_extend) { ++reduction; }
           if (see_value < 0 && mv.is_quiet()) { ++reduction; }
