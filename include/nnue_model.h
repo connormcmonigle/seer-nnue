cc992b08a94855c91f7efbeb7ac91394134d2955
connormcmonigle <connormcmonigle@gmail.com>
2021-09-10 16:16:34 -0700

    tweak

--- include/nnue_model.h
+++ include/nnue_model.h
@@ -49,6 +49,7 @@ struct weights {
   big_affine<T, half_pawn_numel, p_base_dim> p_b{};
   stack_affine<T, 2 * base_dim, 16> fc0{};
   stack_affine<T, 2 * p_base_dim, 16> p_fc0{};
+  stack_affine<T, 16, 16> p_fc1{};
   stack_affine<T, 16, 16> fc1{};
   stack_affine<T, 32, 16> fc2{};
   stack_affine<T, 48, 1> fc3{};
@@ -57,7 +58,7 @@ struct weights {
 
   size_t num_parameters() const {
     return w.num_parameters() + b.num_parameters() + p_w.num_parameters() + p_b.num_parameters() + fc0.num_parameters() + p_fc0.num_parameters() +
-           fc1.num_parameters() + fc2.num_parameters() + fc3.num_parameters();
+           p_fc1.num_parameters() + fc1.num_parameters() + fc2.num_parameters() + fc3.num_parameters();
   }
 
   template <typename streamer_type>
@@ -68,6 +69,7 @@ struct weights {
     p_b.load_(ws);
     fc0.load_(ws);
     p_fc0.load_(ws);
+    p_fc1.load_(ws);
     fc1.load_(ws);
     fc2.load_(ws);
     fc3.load_(ws);
@@ -106,7 +108,8 @@ struct p_eval : chess::sided<p_eval<T>, feature_transformer<T, half_pawn_numel, 
     const auto w_x = white.active();
     const auto b_x = black.active();
     const auto x0 = pov ? splice(w_x, b_x).apply_(relu<T>) : splice(b_x, w_x).apply_(relu<T>);
-    return weights_->p_fc0.forward(x0);
+    const auto x1 = weights_->p_fc0.forward(x0).apply_(relu<T>);
+    return weights_->p_fc1.forward(x1);
   }
 
   p_eval(const weights<T>* src) : weights_{src}, white{&src->p_w}, black{&src->p_b} {}
