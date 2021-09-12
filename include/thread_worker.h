738239553b99eb65fea2ba1770f7bf50724b3b6d
connormcmonigle <connormcmonigle@gmail.com>
2021-09-11 19:15:06 -0700

    test

--- include/thread_worker.h
+++ include/thread_worker.h
@@ -318,6 +318,8 @@ struct thread_worker {
     const bool should_iir = !maybe.has_value() && !ss.has_excluded() && depth >= external.constants->iir_depth();
     if (should_iir) { --depth; }
 
+    if (!is_check && !is_pv) { --depth; }
+
     // step 5. compute static eval and adjust appropriately if there's a tt hit
     const auto [static_value, value] = [&] {
       const auto maybe_eval = internal.cache.find(bd.hash());
