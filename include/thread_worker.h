bb825fb07ae72390ae2ed41acc3f70014bd72ae7
connormcmonigle <connormcmonigle@gmail.com>
2021-10-28 08:40:49 -0700

    pruning-in-check (#83)
    bench: 1830522
    ELO   | 5.06 +- 3.41 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 10432 W: 1446 L: 1294 D: 7692
    ELO   | 2.00 +- 1.32 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 3.00 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 103352 W: 20367 L: 19772 D: 63213

--- include/thread_worker.h
+++ include/thread_worker.h
@@ -412,7 +412,7 @@ struct thread_worker {
 
       const board bd_ = bd.forward(mv);
 
-      const bool try_pruning = !is_root && !is_check && !bd_.is_check() && idx >= 2 && best_score > search::max_mate_score;
+      const bool try_pruning = !is_root && !bd_.is_check() && idx >= 2 && best_score > search::max_mate_score;
 
       // step 11. pruning
       if (try_pruning) {
