d325e6b40f248551fdf30a5752952d7eaa28119e
connormcmonigle <connormcmonigle@gmail.com>
2021-12-02 11:48:28 -0800

    test bench: 4247756

--- include/thread_worker.h
+++ include/thread_worker.h
@@ -425,8 +425,8 @@ struct thread_worker {
 
         if (lm_prune) { break; }
 
-        const bool futility_prune =
-            mv.is_quiet() && depth <= external.constants->futility_prune_depth() && value + external.constants->futility_margin(depth) < alpha;
+        const bool futility_prune = !bd_.is_check() && mv.is_quiet() && depth <= external.constants->futility_prune_depth() &&
+                                    value + external.constants->futility_margin(depth) < alpha;
 
         if (futility_prune) { continue; }
 
