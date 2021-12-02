9ab6e465c61e8a62ade712a1f71faa782546df8d
Connor McMonigle <connormcmonigle@gmail.com>
2021-12-02 14:12:16 -0800

    test bench: 4208208

--- include/thread_worker.h
+++ include/thread_worker.h
@@ -472,6 +472,8 @@ struct thread_worker {
           if (excluded_score >= beta) { multicut = true; }
         }
 
+        if (bd_.is_check() && idx >= 4) { return 1; }
+
         return 0;
       }();
 
