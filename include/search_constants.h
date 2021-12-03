a6b22146b1b0baf3d210c6b0f779a60dc32d4814
Connor McMonigle <connormcmonigle@gmail.com>
2021-12-02 17:52:36 -0800

    test bench: 3948026

--- include/search_constants.h
+++ include/search_constants.h
@@ -106,7 +106,7 @@ struct fixed_constants {
 
   constexpr see_type nmp_see_threshold() const { return 200; }
 
-  constexpr depth_type singular_extension_depth_margin() const { return 2; }
+  constexpr depth_type singular_extension_depth_margin() const { return 3; }
 
   constexpr depth_type singular_search_depth(const depth_type& depth) const { return depth / 2 - 1; }
 
