39dbdbad995b4347dce64bc466b6ad7d9adc3afe
Connor McMonigle <connormcmonigle@gmail.com>
2022-08-12 23:06:51 -0700

    test bench: 3869391

--- include/search_worker.h
+++ include/search_worker.h
@@ -321,8 +321,9 @@ struct search_worker {
                               (entry.bound() == bound_type::upper && entry.score() <= alpha));
       if (is_cutoff) { return make_result(entry.score(), entry.best_move()); }
 
-      const bool should_prompt_research = !is_pv && (entry.depth() + 1 >= depth) && (entry.bound() == bound_type::upper) &&
-                                          (entry.score() + 128 <= alpha) && is_player(reducer, !bd.turn());
+      const bool should_prompt_research = !is_pv && (entry.depth() + 1 >= depth) &&
+                                          (entry.bound() == bound_type::exact || entry.bound() == bound_type::upper) &&
+                                          (entry.score() + 128 <= alpha);
       if (should_prompt_research) { return make_result(alpha, chess::move::null()); }
     }
 
