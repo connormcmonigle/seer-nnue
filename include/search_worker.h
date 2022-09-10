1f626521df13d2828c16793a8feb822f744a88f1
Connor McMonigle <connormcmonigle@gmail.com>
2022-09-10 15:15:03 -0700

    test bench: 3929909

--- include/search_worker.h
+++ include/search_worker.h
@@ -384,15 +384,18 @@ struct search_worker {
       if (nmp_score >= beta) { return make_result(nmp_score, chess::move::null()); }
     }
 
-    const bool try_probcut = !is_pv && depth >= 5 && !ss.has_excluded() && !maybe.has_value();
+    const score_type probcut_beta = beta + 512;
+    const depth_type probcut_depth = depth - 4;
+    const bool try_probcut = !is_pv && depth >= 5 &&
+                             !(maybe.has_value() && maybe->depth() >= probcut_depth &&
+                               (maybe->bound() == bound_type::upper || maybe->bound() == bound_type::exact) && maybe->score() < probcut_beta);
 
     if (try_probcut) {
-      const score_type probcut_beta = beta + 512;
-      const depth_type probcut_depth = depth - 4;
       move_orderer<chess::generation_mode::noisy_and_check> orderer(move_orderer_data(&bd, &internal.hh.us(bd.turn())));
 
       for (const auto& [idx, mv] : orderer) {
         if (!bd.see_gt(mv, 0)) { break; }
+        if (mv == ss.excluded()) { continue; }
 
         ss.set_played(mv);
         nnue::eval_node eval_node_ = eval_node.dirty_child(&bd, mv);
