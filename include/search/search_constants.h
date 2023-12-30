960a5120831304c3f11192159b32d78730a70941
connormcmonigle <connormcmonigle@gmail.com>
2023-12-30 11:18:07 -0800

    spsa-7k-test (#184)
    Elo   | 6.78 +- 5.52 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.97 (-2.94, 2.94) [-1.00, 3.00]
    Games | N: 7124 W: 1742 L: 1603 D: 3779
    Penta | [13, 762, 1880, 887, 20]
    http://chess.grantnet.us/test/34894/
    bench: 4148985

--- include/search/search_constants.h
+++ include/search/search_constants.h
@@ -77,12 +77,12 @@ struct fixed_search_constants {
 
   [[nodiscard]] const std::size_t& thread_count() const noexcept { return thread_count_; }
 
-  [[nodiscard]] constexpr depth_type reduce_depth() const noexcept { return 1; }
+  [[nodiscard]] constexpr depth_type reduce_depth() const noexcept { return 2; }
   [[nodiscard]] constexpr depth_type aspiration_depth() const noexcept { return 4; }
   [[nodiscard]] constexpr depth_type nmp_depth() const noexcept { return 3; }
   [[nodiscard]] constexpr depth_type lmp_depth() const noexcept { return 7; }
   [[nodiscard]] constexpr depth_type snmp_depth() const noexcept { return 6; }
-  [[nodiscard]] constexpr depth_type futility_prune_depth() const noexcept { return 6; }
+  [[nodiscard]] constexpr depth_type futility_prune_depth() const noexcept { return 5; }
   [[nodiscard]] constexpr depth_type quiet_see_prune_depth() const noexcept { return 9; }
   [[nodiscard]] constexpr depth_type noisy_see_prune_depth() const noexcept { return 6; }
   [[nodiscard]] constexpr depth_type singular_extension_depth() const noexcept { return 6; }
@@ -95,10 +95,10 @@ struct fixed_search_constants {
   }
 
   [[nodiscard]] constexpr depth_type nmp_reduction(const depth_type& depth, const score_type& beta, const score_type& value) const noexcept {
-    return 3 + depth / 4 + std::min(3, (value - beta) / 242);
+    return 3 + depth / 4 + std::min(4, (value - beta) / 242);
   }
 
-  [[nodiscard]] constexpr see_type nmp_see_threshold() const noexcept { return 209; }
+  [[nodiscard]] constexpr see_type nmp_see_threshold() const noexcept { return 226; }
 
   [[nodiscard]] constexpr depth_type singular_extension_depth_margin() const noexcept { return 3; }
 
@@ -108,16 +108,16 @@ struct fixed_search_constants {
     return tt_score - 2 * static_cast<score_type>(depth);
   }
 
-  [[nodiscard]] constexpr score_type singular_double_extension_margin() const noexcept { return 165; }
+  [[nodiscard]] constexpr score_type singular_double_extension_margin() const noexcept { return 166; }
 
   [[nodiscard]] constexpr score_type futility_margin(const depth_type& depth) const noexcept {
-    constexpr score_type m = 1541;
+    constexpr score_type m = 1544;
     return m * static_cast<score_type>(depth);
   }
 
   [[nodiscard]] constexpr score_type snmp_margin(const bool& improving, const bool& threats, const depth_type& depth) const noexcept {
-    constexpr score_type m = 292;
-    constexpr score_type b = 118;
+    constexpr score_type m = 297;
+    constexpr score_type b = 112;
     return m * static_cast<score_type>(depth - (improving && !threats)) + (threats ? b : 0);
   }
 
@@ -127,25 +127,25 @@ struct fixed_search_constants {
     return improving ? improving_counts[depth] : worsening_counts[depth];
   }
 
-  [[nodiscard]] constexpr see_type quiet_see_prune_threshold(const depth_type& depth) const noexcept { return -59 * static_cast<see_type>(depth); }
-  [[nodiscard]] constexpr see_type noisy_see_prune_threshold(const depth_type& depth) const noexcept { return -124 * static_cast<see_type>(depth); }
+  [[nodiscard]] constexpr see_type quiet_see_prune_threshold(const depth_type& depth) const noexcept { return -54 * static_cast<see_type>(depth); }
+  [[nodiscard]] constexpr see_type noisy_see_prune_threshold(const depth_type& depth) const noexcept { return -111 * static_cast<see_type>(depth); }
 
   [[nodiscard]] constexpr counter_type history_prune_threshold(const depth_type& depth) const noexcept {
-    return -1357 * static_cast<counter_type>(depth * depth);
+    return -1397 * static_cast<counter_type>(depth * depth);
   }
 
   [[nodiscard]] constexpr depth_type history_reduction(const counter_type& history_value) const noexcept {
     constexpr depth_type limit = 2;
-    const depth_type raw = -static_cast<depth_type>(history_value / 5615);
+    const depth_type raw = -static_cast<depth_type>(history_value / 5872);
     return std::clamp(raw, -limit, limit);
   }
 
   [[nodiscard]] constexpr score_type delta_margin() const noexcept {
-    constexpr score_type margin = 514;
+    constexpr score_type margin = 506;
     return margin;
   }
 
-  [[nodiscard]] constexpr see_type good_capture_prune_see_margin() const noexcept { return 309; }
+  [[nodiscard]] constexpr see_type good_capture_prune_see_margin() const noexcept { return 270; }
   [[nodiscard]] constexpr score_type good_capture_prune_score_margin() const noexcept { return 265; }
 
   [[nodiscard]] constexpr depth_type probcut_search_depth(const depth_type& depth) const noexcept { return depth - 3; }
@@ -155,7 +155,7 @@ struct fixed_search_constants {
     thread_count_ = thread_count;
     for (depth_type depth{1}; depth < lmr_tbl_dim; ++depth) {
       for (depth_type played{1}; played < lmr_tbl_dim; ++played) {
-        lmr_tbl[depth * lmr_tbl_dim + played] = static_cast<depth_type>(0.5354240371831789 + std::log(depth) * std::log(played) / 2.492572118395889);
+        lmr_tbl[depth * lmr_tbl_dim + played] = static_cast<depth_type>(0.4711025619218822 + std::log(depth) * std::log(played) / 2.5650906222921046);
       }
     }
     return *this;
