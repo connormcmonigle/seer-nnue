4291dab7e8667147429c4913e00656f43e5e6105
Connor McMonigle <connormcmonigle@gmail.com>
2026-01-05 20:38:57 -0800

    test bench: 4492339

--- include/search/eval_correction_history.h
+++ include/search/eval_correction_history.h
@@ -72,7 +72,7 @@ template <typename... Ts>
 template <std::size_t N>
 struct composite_eval_correction_history {
   static constexpr depth_type lookup_table_size = 32;
-  static constexpr score_type saturation_threshold = 2048;
+  static constexpr score_type saturation_threshold = 1280;
 
   static constexpr std::array<score_type, lookup_table_size> alpha_lookup_table = [] {
     std::array<score_type, lookup_table_size> result{};
